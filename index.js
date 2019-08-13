766371a8e857e25da97ab7886a8b336666f0c9d8
dark_mift <darkmift@gmail.com>
2019-08-13 21:35:26 +0300

    1st commit basic enviroment set

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+var path = require('path');
+var public = path.join(__dirname, 'public');
+
+
+
+app.use(express.static(public));
+
+const PORT = process.env.PORT || 5000;
+console.log("TCL: PORT", PORT)
+
+app.listen(PORT, () => console.log(`Server running on ${PORT}`))
